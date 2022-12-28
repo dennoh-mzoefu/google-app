5b3db8ad38bfff7548cf8c45e5494a182f0c142e
Denis Gichana <denisgichana162@gmail.com>
2022-12-28 13:24:10 +0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  // purge:[],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: [],
+  },
+  plugins: [],
+};

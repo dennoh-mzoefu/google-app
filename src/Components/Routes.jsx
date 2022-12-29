df4c8921b77db8255106dad8d8b3fc4366df28ab
Denis Gichana <denisgichana162@gmail.com>
2022-12-29 10:40:24 +0300

    WIP

--- src/Components/Routes.jsx
+++ src/Components/Routes.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Routes, Route, Navigate, useRoutes } from "react-router-dom";
+import Results from "./Results";
 
-function Routes() {
-  return <div>Routes</div>;
+function AppRoutes() {
+  const element = useRoutes([
+    {
+      path: "/search",
+      element: <Results />,
+    },
+    {
+      path: "/news",
+      element: <Results />,
+    },
+    {
+      path: "/videos",
+      element: <Results />,
+    },
+    {
+      path: "/images",
+      element: <Results />,
+    },
+  ]);
+  return (
+    <div>
+      {element}
+      <Routes>
+        <Route exact path="/" element={<Navigate replace to="/search" />} />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </div>
+  );
 }
 
-export default Routes;
+export default AppRoutes;

f53be00347c211d7c8d0baa453cad8c1e15e3109
Denis Gichana <denisgichana162@gmail.com>
2022-12-29 10:41:29 +0300

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { useState } from "react";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import AppRoutes from "./Components/Routes";
+import Footer from "./Components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [darkTheme, setDarkTheme] = useState(false);
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <div className={darkTheme ? "dark" : ""}>
+        <div className="min-h-screen bg-slate-100 dark:bg-gradient-to-r from-slate-900 via-slate-900/85 to-slate-900/80">
+          <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+          <AppRoutes />
+          <Footer />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;

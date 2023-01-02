90cb482e414989c045f94791b491164138232ae4
Denis Gichana <denisgichana162@gmail.com>
2023-01-02 13:33:57 +0300

    search functionality

--- src/Components/KnowledgePanel.jsx
+++ src/Components/KnowledgePanel.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { StateContext } from "../Context/StateContextProvider";
 
 function KnowledgePanel() {
-  return <div>KnowledgePanel</div>;
+  const { results, loading, getResults, searchTerm } = useContext(StateContext);
+  return (
+    <div>
+      <div className="mt-6">
+        <h1 className="text-slate-900 text-2xl dark:text-slate-50">
+          {results?.data?.knowledge_panel?.name}
+        </h1>
+        <h3 className="dark:text-slate-400">
+          {results?.data?.knowledge_panel?.label}
+        </h3>
+      </div>
+      <img
+        className="rounded-lg mb-4"
+        src={results?.data?.knowledge_panel?.image?.url}
+      />
+    </div>
+  );
 }
 
 export default KnowledgePanel;

dc1cca2f44760614fd30fe40a58e57f4921970de
Denis Gichana <denisgichana162@gmail.com>
2023-01-02 13:33:45 +0300

    search functionality

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function Footer() {
   return (
     <div>
-      <footer className="bg-gray-500 text-white py-5">
+      <footer className="bg-gray-900 text-white py-5">
         <div className="container mx-auto px-4">
           <div className="flex flex-wrap">
             <div className="w-full md:w-1/2 text-center md:text-left py-6">
-              <p className="mb-4">Copyright &copy; 2021 Your Company</p>
+              <p className="mb-4">Copyright &copy; 2021 ,Denis Gichana</p>
             </div>
             <div className="w-full md:w-1/2 text-center md:text-right py-6">
               <a href="#" className="inline-block mr-4 hover:text-gray-400">

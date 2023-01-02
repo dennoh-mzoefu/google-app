818851c380c3b5b7e883e6d8da8ef9da290776b1
Denis Gichana <denisgichana162@gmail.com>
2023-01-02 13:35:37 +0300

    search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className={darkTheme ? "dark" : ""}>
-        <div className="min-h-screen bg-slate-100 dark:bg-gradient-to-r from-slate-900 via-slate-900/85 to-slate-900/80">
+        <div className=" min-h-screen bg-slate-100 dark:bg-gradient-to-r from-slate-900 via-slate-900/85 to-slate-900/80">
           <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
           <AppRoutes />
           <Footer />

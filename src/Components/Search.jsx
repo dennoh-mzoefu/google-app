4aa0e9f8ac796dd67d7f4fb6dc747c239164ba88
Denis Gichana <denisgichana162@gmail.com>
2023-01-02 13:35:09 +0300

    search functionality

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -5,14 +5,19 @@ import { StateContext } from "../Context/StateContextProvider";
 // import { useStateContext } from "../Context/StateContextProvider";
 
 const Search = () => {
-  const { setSearchTerm } = useContext(StateContext);
+  const { setSearchTerm, getResults } = useContext(StateContext);
   const [text, setText] = useState("Elon Musk");
   const [debouncedValue] = useDebounce(text, 300);
 
   useEffect(() => {
     if (debouncedValue) setSearchTerm(debouncedValue);
   }, [debouncedValue]);
-
+  const handleResults = () => {
+    setTimeout(() => {
+      getResults();
+      // alert("J'aime les chats");
+    }, 3000);
+  };
   return (
     <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
       <input
@@ -21,6 +26,8 @@ const Search = () => {
         className="sm:w-96 w-80 h-10 dark:bg-gray-200  border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
         placeholder="🔎 Search Google or type URL"
         onChange={(e) => setText(e.target.value)}
+        onKeyPress={(e) => e.key === "Enter" && handleResults()}
+        onKeyDown={(e) => e.key === "Enter" && handleResults()}
       />
       {text !== "" && (
         <button
